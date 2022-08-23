8e734330d4dd5a04dbff64d68182f0cd20f6fb8a
aida_atz <attarzadehosseiniaida@gmail.com>
2022-08-23 23:21:47 +0430

    add popState event

--- assets/js/router.js
+++ assets/js/router.js
@@ -4,15 +4,16 @@ import routerView from "./elements/routerView.js";
 export class createRouter{
     constructor(data) {
         this.routes=data.routes;
+        this.historyState = { value: history.state };
         window.router=this;
         window.customElements.define("router-link",routerLink);
         window.customElements.define("router-view",routerView);
-        window.addEventListener('hashchange', ()=>{
-        
-        });
-        window.addEventListener('load', ()=>{
-        
-        });
+        window.addEventListener('popstate',({state})=>{
+            const to=location.pathName;
+            const from = state.back;
+            console.log(history)
+            console.log(state)
+        })
     }
     renderComponent(component){
         const list = document.getElementsByTagName("router-view")[0];
@@ -20,16 +21,18 @@ export class createRouter{
             list.innerHTML=response.default.template;
         });
     };
-    changeLocation(to , replace=false){
-        window.history[replace ? "replaceState" : "pushState"](null,null,to);
+    changeLocation(to,state , replace=false){
+        window.history[replace ? "replaceState" : "pushState"](state,null,to);
     }
-    push(route){
-        this.changeLocation(route.path)
-        this.renderComponent(route.component);
+    push(to){
+        const currentState=Object.assign({},this.historyState,history.state,{forward:to})
+        const state = Object.assign({},{back:location.pathname,current:to.path,forward:null},{position:currentState.position? currentState.position+1 :1})
+        this.changeLocation(to.path , state)
+        this.renderComponent(to.component);
     };
-    replace(route){
-        this.changeLocation(route.path , true);
-        this.renderComponent(route.component);
+    replace(to){
+        this.changeLocation(to.path , true);
+        this.renderComponent(to.component);
     };
     next(){
         
